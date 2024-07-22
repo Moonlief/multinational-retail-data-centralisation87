ae5887b9a84a9ea9260b5978b04c39e3900b969e
Moonlie <moonlie.fong@gmail.com>
2024-07-22 19:27:00 +0100

    s3, trouble cleaning the data and converting into float

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,6 +1,7 @@
 
 
 import pandas as pd
+import re
 
 
 class DataCleaning:
@@ -41,6 +42,48 @@ class DataCleaning:
 
         return clean_pdf
     
+    def called_clean_store_data(self, df_api):
+
+        print("Checking for Null values:")
+        pdf_na = df_api.isna().mean()*100
+        print(pdf_na)
+
+        print("Deleting rows with more than 4 Null values")
+        drop_pdf =df_api.dropna(axis=0, thresh=4)
+        print(drop_pdf)
+
+        print("Looking for duplicates:")
+        print(df_api.duplicated())
+
+        print("checking data types for each columns")
+        print(df_api.info())
+
+        return df_api
+
+
+    def convert_product_weights(self, csv_df):
+        def convert_weight(weight):
+            weight = re.sub(r'[^\d.]+', '', str(weight))
+            
+            if 'kg' in weight:
+                con_weight = weight.replace('kg', '').strip()
+                return float(con_weight)
+            elif 'g' in weight:
+                con_weight = weight.replace('g', '').strip()
+
+                return float(con_weight)/1000
+            elif 'ml' in weight:
+                con_weight = weight.replace('ml', '').strip()
+                return float(con_weight)/1000
+            else:
+                return float(weight) 
+
+        csv_df['weight'] = csv_df['weight'].apply(convert_weight)
+        print(csv_df['weight'].isna().sum())
+        csv_df['weight'].fillna(csv_df['weight'].mean(), inplace=True)
+        return csv_df
+
+
 
 
 
--- data_extraction.py
+++ data_extraction.py
@@ -1,6 +1,8 @@
 
 import pandas as pd
 import tabula
+import requests
+import boto3
 
 
 
@@ -21,11 +23,47 @@ class DataExtractor:
     
     def retrieve_pdf_data(self, pdf_link):
         read_pdf = tabula.read_pdf(pdf_link, pages="all")
-        print(read_pdf)
         pdf_df_list = [pd.DataFrame(page) for page in read_pdf]
         pdf_df = pd.concat(pdf_df_list, ignore_index=True)
-        self.connector.close()
         return pdf_df
+    
+    def list_number_of_stores(self,endpoint, headers):
+        response = requests.get(endpoint, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            number_of_stores = len(data)
+            print(number_of_stores)
+
+            return print(data.keys())
+        
+        else:
+            print(f"Request failed with status code: {response.status_code}")
+            print(f"Response Text: {response.text}")
+            return response.text
+    
+    
+    def retrieve_stores_data(self, endpoint, headers, number_of_stores):
+        stores_data = []
+        for store_number in range(1, number_of_stores + 1):
+            response = requests.get(endpoint.format(store_number=store_number), headers=headers)
+            if response.status_code == 200:
+                stores_data.append(response.json())
+                api_df = pd.DataFrame(stores_data)
+                return api_df
+
+            else:
+                return response.raise_for_status()
+    
+    def extract_from_s3(self,s3_bucket, s3_object_key,local_file_path):
+        s3 = boto3.client('s3')
+        s3.download_file(s3_bucket, s3_object_key, local_file_path)
+        df_csv = pd.read_csv(s3_object_key)
+        df_s3 = pd.DataFrame(df_csv)
+        return df_s3
+    
+
+
+
 
         
 
